db40eb220f67e8e1e4c7b6a5082e4fe1ba5ae53a
Redstonia <redstonia1234@gmail.com>
2022-02-24 17:23:05 +1300

    Add project files.

---
+++ WordleSolver/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordleSolver
+{
+    class Program
+    {
+        public static int Guess = 0;
+        public static char[] Alphabet = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (char)i).ToArray();
+        public static char[] CommonLetters = "eariotnslcupdmhgbfywkvxzjq".ToCharArray();
+
+        public static Correctness[] WordAccuracy = new Correctness[26];
+        public static int[] Position = new int[26];
+        public static bool[,] IncorrectPositions = new bool[26, 5];
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wordle Solver");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Guess++;
+                string word = GuessWord();
+                Console.WriteLine();
+                Console.WriteLine($"Guess: {word.ToUpperInvariant()}");
+                
+                Console.Write("Input results of guess in gyn: ");
+                string gyn = Console.ReadLine();
+                for (int j = 0; j < 5; j++)
+                {
+                    int letterindex = Array.IndexOf(Alphabet, word[j].ToString().ToLowerInvariant()[0]);
+                    if (gyn[j] == 'g')
+                    {
+                        WordAccuracy[letterindex] = Correctness.CorrectPosition;
+                        Position[letterindex] = j;
+                    }
+
+                    else if (gyn[j] == 'y')
+                    {
+                        if (WordAccuracy[letterindex] != Correctness.CorrectPosition)
+                        {
+                            WordAccuracy[letterindex] = Correctness.InWord;
+                        }
+
+                        IncorrectPositions[letterindex, j] = true;
+                    }
+
+                    else if (gyn[j] == 'n')
+                    {
+                        WordAccuracy[letterindex] = Correctness.NotInWord;
+                    }
+
+                    else
+                    {
+                        throw new Exception("burn in hell");
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        public static string GuessWord()
+        {
+            if (Guess == 1)
+            {
+                return "STARE";
+            }
+
+            return WordList.ProvideGuess();
+        }
+    }
+
+    public enum Correctness
+    {
+        None,
+        NotInWord,
+        InWord,
+        CorrectPosition
+    }
+}
---
+++ WordleSolver/WordList.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WordleSolver
+{
+    class WordList
+    {
+        public static string[] Answers = File.ReadAllLines("lists/answers.txt");
+        public static string[] ValidGuesses = File.ReadAllLines("lists/validguesses.txt").Concat(Answers).ToArray();
+
+        public static bool IsValidGuess(string word)
+        {
+            return ValidGuesses.Contains(word.ToLowerInvariant());
+        }
+
+        public static bool IsAnswer(string word)
+        {
+            return Answers.Contains(word.ToLowerInvariant());
+        }
+
+        public static string ProvideGuess()
+        {
+            List<string> perfectConditions = new List<string>();
+
+            for (int i = 0; i < Answers.Length; i++)
+            {
+                string potentialWord = Answers[i];
+                if (CheckWord(potentialWord))
+                {
+                    perfectConditions.Add(potentialWord);
+                }
+            }
+
+            Console.WriteLine("ALL POTENTIAL ANSWERS");
+            int currentHighestPriority = int.MaxValue;
+            string highestCandidate = "";
+
+            foreach (string s in perfectConditions)
+            {
+                Console.Write($"  {s.ToUpperInvariant()}");
+
+                bool doubled = false;
+                foreach (char c in s)
+                {
+                    if (s.Count(f => f == c) > 1)
+                    {
+                        Console.WriteLine(" (double)");
+                        doubled = true;
+                        break;
+                    }
+                }
+
+                if (doubled)
+                {
+                    continue;
+                }
+
+                Console.WriteLine();
+
+                int priority = 0;
+                foreach (char character in s)
+                {
+                    priority += Array.IndexOf(Program.CommonLetters, character);
+                }
+
+                if (priority < currentHighestPriority)
+                {
+                    currentHighestPriority = priority;
+                    highestCandidate = s;
+                }
+            }
+
+            return highestCandidate;
+        }
+
+        private static bool CheckWord(string word)
+        {
+            for (int i = 0; i < Program.Alphabet.Length; i++)
+            {
+                if (Program.WordAccuracy[i] == Correctness.CorrectPosition)
+                {
+                    if (word[Program.Position[i]] != Program.Alphabet[i])
+                    {
+                        return false;
+                    }
+                }
+
+                else if (Program.WordAccuracy[i] == Correctness.InWord)
+                {
+                    if (!word.Contains(Program.Alphabet[i]))
+                    {
+                        return false;
+                    }
+
+                    else if (Program.IncorrectPositions[i, word.IndexOf(Program.Alphabet[i])])
+                    {
+                        return false;
+                    }
+                }
+
+                else if (Program.WordAccuracy[i] == Correctness.NotInWord)
+                {
+                    if (word.Contains(Program.Alphabet[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
